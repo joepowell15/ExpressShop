49d08822b3c516a90f06b58adad801e37a294f0f
Jovaughn Powell <jovaughnpowell@my.unt.edu>
2020-07-09 22:12:28 -0500

    server side validation

--- public/js/schemas/loginSchema.js
+++ public/js/schemas/loginSchema.js
@@ -1,9 +1,9 @@
 const Joi = require("@hapi/joi");
 
 const loginSchema = Joi.object({
-  username: Joi.string().alphanum().min(3).max(30).required(),
+  username: Joi.string().alphanum().min(3).max(30).trim().required(),
 
-  password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{6,30}$")).required(),
+  password: Joi.string().min(6).max(30).trim().required(),
 });
 
 module.exports = loginSchema;
