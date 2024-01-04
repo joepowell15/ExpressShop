474aec36d03febf056b54cb72670bcb95dbe5a2f
joepowell15 <42976784+joepowell15@users.noreply.github.com>
2024-01-03 18:46:55 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,20 @@
 module.exports = {
-    app: {
-        name: "app",
-        script: "../../app.js",
-        env: {
-            NODE_ENV: "dev",
-            SOCKET_POLLING_PORT :5000,
-            RETHINKDB_HOST: "146.190.40.202",
-            RETHINKDB_PORT: 28015,
-            RETHINKDB_DEFAULT_DB: "StoreDB",
-        }
+  apps : {
+    name   : "Portfolio",
+    script : "./app.js",
+    env_production: {
+        NODE_ENV: "prod",
+        SOCKET_POLLING_PORT : 5000,
+        RETHINKDB_HOST: "",
+        RETHINKDB_PORT: null,
+        RETHINKDB_DEFAULT_DB: "StoreDB",
+    },
+    env_development: {
+        NODE_ENV: "dev",
+        SOCKET_POLLING_PORT : 5000,
+        RETHINKDB_HOST: "146.190.40.202",
+        RETHINKDB_PORT: 28015,
+        RETHINKDB_DEFAULT_DB: "StoreDB",
     }
+  }
 }
